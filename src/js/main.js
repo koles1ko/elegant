dbabb359a6bdfea36c366a5bb5157a4f65c924bc
Dmitry Kolesnikov <34190412+koles1ko@users.noreply.github.com>
2020-02-13 22:42:01 +0300

    try to make menu

--- src/js/main.js
+++ src/js/main.js
@@ -7,12 +7,28 @@ $(function () {
   //   $('.menu__list').slideToggle();
   // }); Для бургера!
 
-  //   $('.class').slick({
-  //   dots: true,
-  //   arrows: false,
-  //   slidesToShow: 4,
-  //   slidesToScroll: 4,
-  // }); Обычный одиночный слайдер!
+  // $(".test").on('click', function(){
+  //   $(".test").toggleClass(".active");
+    
+  // }); 
+
+  // document.getElementById('test').onclick = function() {
+  //   document.getElementById('test').classList.add('active');
+  // }
+
+  $("#test").click(function() {
+    $("#test").removeClass("active");         
+    $(this).toggleClass("active");
+  })
+
+
+    $('.gallery').slick({
+    dots: true,
+    arrows: false,
+    slidesToShow: 4,
+    slidesToScroll: 4,
+  }); 
+  //Обычный одиночный слайдер!
 
   //  $('.slider-for').slick({
   //   slidesToShow: 1,
@@ -39,5 +55,7 @@ $(function () {
   //   $('#'+id).addClass('active-tab').fadeIn();
   //   return false;
   // }); Для табов!
-
+  
+  
+ 
 });
